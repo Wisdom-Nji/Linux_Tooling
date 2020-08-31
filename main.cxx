92449b42e48943c628bcf23aeb9baa40a5ec44ca
Sherlock Wisdom <wisdomnji@gmail.com>
2020-08-31 13:44:53 +0100

    creating arrays of strings

--- main.cxx
+++ main.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -6,10 +7,29 @@
 
 using namespace std;
 
+auto best_guesses( const char* text, int state_counter ) {
+	cout << "state counter: " << state_counter << endl;
 
-int rl_custom( int input1, int input2 ) {
+	vector<string> options = { "sherlock", "spike", "shrek", "shit" };
+
+	char* matches[options.size()];
+	for( int i=0;i<options.size();++i) {
+		// matches[i] = options[i].c_str();
+		matches[i] = strdup( options[i].c_str());
+	}
+
+	return matches;
+}
+
+
+auto rl_custom( const char* text, int start, int end ) {
+	// Any number > 0 will disable the default filename listings if matches not found
+	rl_attempted_completion_over = 1;
+
+	// Returns NULL if they are no matches
+	rl_completion_matches( text, best_guesses );
 }
 
 int main( int argc, char** argv ) {
-	int rl_value = rl_bind_key('\t', rl_custom );
+	rl_attempted_completion_function = rl_custom;
 }
